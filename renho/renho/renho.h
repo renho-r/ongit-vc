417dcb6794e1114506a9932d8f9084b6ef742030
renho <rho_i_xx@163.com>
2015-04-10 13:07:36 +0800

    first commit base

---
+++ renho/renho/renho.h
@@ -0,0 +1,32 @@
+
+// renho.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CrenhoApp:
+// See renho.cpp for the implementation of this class
+//
+
+class CrenhoApp : public CWinApp
+{
+public:
+	CrenhoApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CrenhoApp theApp;
